235c520490e0bcc9c2396896d6d5a480462e567c
Hannes Lundberg <hanlu520@student.liu.se>
2024-06-12 23:57:11 +0200

    Personal page done

---
+++ Assignment_1/personal_page/lib/main.dart
@@ -0,0 +1,153 @@
+/* A File for creating a static profile page 
+The code is written by Hannes Lundberg */
+
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(25, 100, 0, 0.1),
+            brightness: Brightness.light),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Profile page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              //Creates a cirle-shaped profile picture
+              child: Container(
+                width: 175,
+                height: 175,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/damen.jpg'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              //Creates a text with the name of the user
+              child: Text(
+                'Hannes Lundberg',
+                style: TextStyle(
+                    color: Colors.black38,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'PT'),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              width: 300,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(25, 100, 0, 0.1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Flutter-learning student',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'PT',
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Icon(Icons.email, color: Colors.black45),
+                      SizedBox(width: 10),
+                      Text(
+                        'hanlu520@liu.se',
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.phone, color: Colors.black45),
+                      SizedBox(width: 10),
+                      Text(
+                        '+46 7012885',
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.web_asset, color: Colors.black45),
+                      SizedBox(width: 10),
+                      Text(
+                        'www.lnu.se',
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
